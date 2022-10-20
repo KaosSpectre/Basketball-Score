8f826e254931e9a48850c4533b3c68ffa338e44d
KaosSpectre <97178572+KaosSpectre@users.noreply.github.com>
2022-10-19 20:37:38 -0400

    JS streamline
    removed onclick event listener
    added addEventListeners instead

--- index.js
+++ index.js
@@ -1,31 +1,46 @@
+// declare point variables
 let homePoints = 0;
 let guestPoints = 0;
 let homeScore = document.getElementById("home-score");
 let guestScore = document.getElementById("guest-score");
+// get btn elements
+btnH1 = document.getElementById("btn-h1");
+btnH2 = document.getElementById("btn-h2");
+btnH3 = document.getElementById("btn-h3");
+btnG1 = document.getElementById("btn-g1");
+btnG2 = document.getElementById("btn-g2");
+btnG3 = document.getElementById("btn-g3");
+newGameEl = document.getElementById("new-game");
+// add btn event listeners
+btnH1.addEventListener("click", function () {
+	addHome(1);
+});
+btnH2.addEventListener("click", function () {
+	addHome(2);
+});
+btnH3.addEventListener("click", function () {
+	addHome(3);
+});
+btnG1.addEventListener("click", function () {
+	addGuest(1);
+});
+btnG2.addEventListener("click", function () {
+	addGuest(2);
+});
+btnG3.addEventListener("click", function () {
+	addGuest(3);
+});
+newGameEl.addEventListener("click", function () {
+	newGame();
+});
 // add points to homePoints
-function add1Home() {
-	homePoints += 1;
-	homeScore.textContent = homePoints;
-}
-function add2Home() {
-	homePoints += 2;
-	homeScore.textContent = homePoints;
-}
-function add3Home() {
-	homePoints += 3;
+function addHome(points) {
+	homePoints += points;
 	homeScore.textContent = homePoints;
 }
 // add points to guestScore
-function add1Guest() {
-	guestPoints += 1;
-	guestScore.textContent = guestPoints;
-}
-function add2Guest() {
-	guestPoints += 2;
-	guestScore.textContent = guestPoints;
-}
-function add3Guest() {
-	guestPoints += 3;
+function addGuest(points) {
+	guestPoints += points;
 	guestScore.textContent = guestPoints;
 }
 // reset both scores to 0 when newGame runs
@@ -34,4 +49,10 @@ function newGame() {
 	homePoints = 0;
 	guestScore.textContent = 0;
 	guestPoints = 0;
+}
+
+if (homePoints > guestPoints) {
+	document.getElementById("outline-home").className = ".outline";
+} else if (homePoints < guestPoints) {
+	document.getElementById("outline-guest").className = ".outline";
 }
